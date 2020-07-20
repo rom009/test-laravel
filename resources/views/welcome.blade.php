a5a23eb1ed80012949667273ead41ad140079e66
rom009 <66899676+rom009@users.noreply.github.com>
2020-07-21 00:52:08 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Rafat is bad
+                   Yoo Bro!
                 </div>
 
                 <div class="links">
