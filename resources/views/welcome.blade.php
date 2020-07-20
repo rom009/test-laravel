89a9b01381285b4db97ec55967b662c94f21e919
rafat ahmed <rafat69ahmed@gmail.com>
2020-07-21 00:05:26 +0600

    final test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Romesh vai the ultimate boss
                 </div>
 
                 <div class="links">
